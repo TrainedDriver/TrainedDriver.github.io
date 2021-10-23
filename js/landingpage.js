a195759fb9f705b98de32e420d836a84403da927
sina_giannis <49285637+sinakhs@users.noreply.github.com>
2021-10-23 20:46:19 +0300

    Update carousel

--- js/landingpage.js
+++ js/landingpage.js
@@ -18,7 +18,18 @@ function scrollFunction() {
 $('.owl-carousel').owlCarousel({
   infinite:true,
   nav: false,
-  items: 4, 
+  items: 4,
+  responsive:{
+    0:{
+        items:1
+    },
+    600:{
+        items:2
+    },
+    1000:{
+        items:4
+    }
+  }
 });
 
 // SCROLLSPY
